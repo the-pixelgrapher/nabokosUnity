6bb2d6d2868d9d137d600db0711ca4d13d5f2e7e
David Morris <pixelgrapher@outlook.co.nz>
2020-02-09 16:42:24 +1300

    Game manager added

---
+++ Assets/Scripts/General/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public bool levelComplete;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
